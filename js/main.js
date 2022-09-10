0baddb40a50e51b03a40758b9686937918ed3148
Cod Mark <95895380+CodMark@users.noreply.github.com>
2022-09-10 18:44:00 +0530

    movie download

--- js/main.js
+++ js/main.js
@@ -32,3 +32,24 @@
         localStorage.setItem("dark-mode", true);
     }
 })();
+
+
+// Get the button
+let mybutton = document.getElementById("movetoo");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.bottom = "6px";
+    } else {
+        mybutton.style.bottom = "-46px";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
